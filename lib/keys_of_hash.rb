56c9b8427d2c839e7a2ac8fbd015baf085f42160
elainapolson <elainapolson@gmail.com>
2015-08-31 15:31:20 -0400

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+
+  def keys_of(*arguments)
+    matches = []
+    arguments.each do |arg|
+      self.each do |key, value|      
+        if arg == value
+          matches << key
+        end
+      end
+    end
+    matches
   end
 end
+
+
+# Given a hash, return an array with every key from the hash whose value 
+# matches the value(s) given as an argument. You will have to use the 
+# splat (*) operator. Read more about that below the example
